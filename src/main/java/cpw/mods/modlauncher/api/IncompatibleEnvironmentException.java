182c618b9a5dcd0f53d72884a1adb2e42e25db46
cpw <cpw@weeksfamily.ca>
2017-10-06 22:21:02 -0400

    Add message to IncompatibleEnvException

--- src/main/java/cpw/mods/modlauncher/api/IncompatibleEnvironmentException.java
+++ src/main/java/cpw/mods/modlauncher/api/IncompatibleEnvironmentException.java
@@ -4,5 +4,7 @@ package cpw.mods.modlauncher.api;
  * Indicate an incompatible environment to the modlauncher system.
  */
 public class IncompatibleEnvironmentException extends Exception {
-
+    public IncompatibleEnvironmentException(String message) {
+        super(message);
+    }
 }
