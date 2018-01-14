98028a3e2818e697d88575691bb324bd88bf68bf
cpw <cpw@weeksfamily.ca>
2018-01-14 16:20:22 -0500

    Don't force logging to INFO level always.

--- src/main/java/cpw/mods/modlauncher/Logging.java
+++ src/main/java/cpw/mods/modlauncher/Logging.java
@@ -6,8 +6,4 @@ import org.apache.logging.log4j.core.config.*;
 public class Logging {
     static final Logger launcherLog = LogManager.getLogger("Launcher");
     static final Marker CLASSLOADING = MarkerManager.getMarker("CLASSLOADING");
-
-    static {
-        Configurator.setRootLevel(Level.INFO);
-    }
 }
