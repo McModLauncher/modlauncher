2ac5413d0fa889aae60b4ed6af32c619fca79e68
cpw <cpw@weeksfamily.ca>
2019-01-01 19:54:55 -0500

    Define packages for everything we load...

--- src/main/java/cpw/mods/modlauncher/TransformingClassLoader.java
+++ src/main/java/cpw/mods/modlauncher/TransformingClassLoader.java
@@ -4,12 +4,13 @@ import cpw.mods.modlauncher.api.ITransformingClassLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.annotation.Nullable;
 import java.io.*;
 import java.net.*;
 import java.nio.file.*;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.function.Predicate;
+import java.util.jar.*;
 import java.util.stream.*;
 
 import static cpw.mods.modlauncher.LogMarkers.*;
@@ -41,7 +42,7 @@ public class TransformingClassLoader extends ClassLoader implements ITransformin
         super();
         this.classTransformer = new ClassTransformer(transformStore, pluginHandler, this);
         this.specialJars = Stream.of(specialJars).map(rethrowFunction(f -> f.toUri().toURL())).toArray(URL[]::new);
-        this.delegatedClassLoader = new DelegatedClassLoader();
+        this.delegatedClassLoader = new DelegatedClassLoader(this);
         this.targetPackageFilter = s -> SKIP_PACKAGE_PREFIXES.stream().noneMatch(s::startsWith);
     }
 
@@ -99,16 +100,35 @@ public class TransformingClassLoader extends ClassLoader implements ITransformin
         InputStream getInputStream() {
             return this.inputStream;
         }
+
+        Manifest getJarManifest() {
+            try {
+                if (this.urlConnection instanceof JarURLConnection) {
+
+                    return ((JarURLConnection) this.urlConnection).getManifest();
+                }
+            } catch (IOException e) {
+                // noop
+            }
+            return null;
+        }
     }
 
-    private class DelegatedClassLoader extends URLClassLoader {
-        DelegatedClassLoader() {
-            super(specialJars);
+    private static class DelegatedClassLoader extends URLClassLoader {
+        static {
+            ClassLoader.registerAsParallelCapable();
+        }
+
+        private final TransformingClassLoader tcl;
+
+        DelegatedClassLoader(TransformingClassLoader cl) {
+            super(cl.specialJars, null);
+            this.tcl = cl;
         }
 
         @Override
         protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
-            return TransformingClassLoader.this.loadClass(name, resolve);
+            return tcl.loadClass(name, resolve);
         }
 
         @Override
@@ -122,6 +142,7 @@ public class TransformingClassLoader extends ClassLoader implements ITransformin
 
             final URL classResource = findResource(path);
             byte[] classBytes;
+            Manifest jarManifest = null;
             if (classResource != null) {
                 try (AutoURLConnection urlConnection = new AutoURLConnection(classResource)) {
                     final int length = urlConnection.getContentLength();
@@ -132,6 +153,7 @@ public class TransformingClassLoader extends ClassLoader implements ITransformin
                         pos += read;
                         remain -= read;
                     }
+                    jarManifest = urlConnection.getJarManifest();
                 } catch (IOException e) {
                     LOGGER.error(CLASSLOADING,"Failed to load bytes for class {} at {}", name, classResource, e);
                     throw new ClassNotFoundException("blargh", e);
@@ -139,9 +161,17 @@ public class TransformingClassLoader extends ClassLoader implements ITransformin
             } else {
                 classBytes = new byte[0];
             }
-            classBytes = classTransformer.transform(classBytes, name);
+            classBytes = tcl.classTransformer.transform(classBytes, name);
             if (classBytes.length > 0) {
                 LOGGER.debug(CLASSLOADING, "Loaded transform target {} from {}", name, classResource);
+
+                int i = name.lastIndexOf('.');
+                String pkgname = i > 0 ? name.substring(0, i) : "";
+                // Check if package already loaded.
+                if (getPackage(pkgname) == null) {
+                    definePackage(pkgname, jarManifest);
+                }
+
                 return defineClass(name, classBytes, 0, classBytes.length);
             } else {
                 LOGGER.debug(CLASSLOADING, "Failed to transform target {} from {}", name, classResource);
@@ -150,5 +180,47 @@ public class TransformingClassLoader extends ClassLoader implements ITransformin
             }
         }
 
+        Package definePackage(String name, @Nullable Manifest man) throws IllegalArgumentException
+        {
+            String path = name.replace('.', '/').concat("/");
+            String specTitle = null, specVersion = null, specVendor = null;
+            String implTitle = null, implVersion = null, implVendor = null;
+
+            if (man != null) {
+                Attributes attr = man.getAttributes(path);
+                if (attr != null) {
+                    specTitle = attr.getValue(Attributes.Name.SPECIFICATION_TITLE);
+                    specVersion = attr.getValue(Attributes.Name.SPECIFICATION_VERSION);
+                    specVendor = attr.getValue(Attributes.Name.SPECIFICATION_VENDOR);
+                    implTitle = attr.getValue(Attributes.Name.IMPLEMENTATION_TITLE);
+                    implVersion = attr.getValue(Attributes.Name.IMPLEMENTATION_VERSION);
+                    implVendor = attr.getValue(Attributes.Name.IMPLEMENTATION_VENDOR);
+                }
+                attr = man.getMainAttributes();
+                if (attr != null) {
+                    if (specTitle == null) {
+                        specTitle = attr.getValue(Attributes.Name.SPECIFICATION_TITLE);
+                    }
+                    if (specVersion == null) {
+                        specVersion = attr.getValue(Attributes.Name.SPECIFICATION_VERSION);
+                    }
+                    if (specVendor == null) {
+                        specVendor = attr.getValue(Attributes.Name.SPECIFICATION_VENDOR);
+                    }
+                    if (implTitle == null) {
+                        implTitle = attr.getValue(Attributes.Name.IMPLEMENTATION_TITLE);
+                    }
+                    if (implVersion == null) {
+                        implVersion = attr.getValue(Attributes.Name.IMPLEMENTATION_VERSION);
+                    }
+                    if (implVendor == null) {
+                        implVendor = attr.getValue(Attributes.Name.IMPLEMENTATION_VENDOR);
+                    }
+                }
+            }
+            return definePackage(name, specTitle, specVersion, specVendor, implTitle, implVersion, implVendor, null);
+        }
+
     }
+
 }
