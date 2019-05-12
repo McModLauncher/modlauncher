6718b7a9b44eef24c8e2f219f0370796c205f007
cpw <cpw+github@weeksfamily.ca>
2019-05-12 16:30:19 -0400

    Docs
    Signed-off-by: cpw <cpw+github@weeksfamily.ca>

--- src/api/java/cpw/mods/modlauncher/serviceapi/ITransformerDiscoveryService.java
+++ src/api/java/cpw/mods/modlauncher/serviceapi/ITransformerDiscoveryService.java
@@ -3,6 +3,15 @@ package cpw.mods.modlauncher.serviceapi;
 import java.nio.file.Path;
 import java.util.List;
 
+/**
+ * Called early in setup, to allow pluggable "discovery" of additional transformer services.
+ * FML uses this to identify transformers in the mods directory (e.g. Optifine) for loading into ModLauncher.
+ */
 public interface ITransformerDiscoveryService {
+    /**
+     * Return a list of additional paths to be added to transformer service discovery during loading.
+     * @param gameDirectory The root game directory
+     * @return The list of services
+     */
     List<Path> candidates(final Path gameDirectory);
 }
