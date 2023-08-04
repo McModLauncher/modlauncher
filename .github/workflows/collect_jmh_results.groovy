28bae66a6ffc45f56757d528300587fa388abb5f
Matyrobbrt <65940752+Matyrobbrt@users.noreply.github.com>
2023-08-04 21:27:17 +0300

    Update

--- .github/workflows/collect_jmh_results.groovy
+++ .github/workflows/collect_jmh_results.groovy
@@ -7,8 +7,8 @@ import java.nio.file.Path
 @GrabResolver(name='jitpack.io', root='https://jitpack.io/')
 @GrabResolver(name = 'central', root='https://repo1.maven.org/maven2/')
 @Grapes([
-    @Grab('org.apache.groovy:groovy-json:4.0.13'),
-    @Grab('com.github.Steppschuh:Java-Markdown-Generator:1.3.2')
+        @Grab('org.apache.groovy:groovy-json:4.0.13'),
+        @Grab('com.github.Steppschuh:Java-Markdown-Generator:1.3.2')
 ])
 
 final dummyClassResults = [:]
@@ -16,21 +16,21 @@ final noopResults = [:]
 
 final resultsPath = Path.of('jmh_results')
 Files.list(resultsPath).map { it.resolve('result.json') }
-    .map { [new JsonSlurper().parse(it.toFile()), it.parent.fileName.toString().substring('jmh_'.length())] }
-    .forEach {
-        dummyClassResults[it[1]] = "${it[0][0].primaryMetric.score.round(2)} ± ${it[0][0].primaryMetric.scoreError.round(2)} ${it[0][0].primaryMetric.scoreUnit}"
-        noopResults[it[1]] = "${it[0][1].primaryMetric.score.round(2)} ± ${it[0][1].primaryMetric.scoreError.round(2)} ${it[0][1].primaryMetric.scoreUnit}"
-    }
+        .map { [new JsonSlurper().parse(it.toFile()), it.parent.fileName.toString().substring('jmh_'.length())] }
+        .forEach {
+            dummyClassResults[it[1]] = "${it[0][0].primaryMetric.score.round(2)} ± ${it[0][0].primaryMetric.scoreError.round(2)} ${it[0][0].primaryMetric.scoreUnit}"
+            noopResults[it[1]] = "${it[0][1].primaryMetric.score.round(2)} ± ${it[0][1].primaryMetric.scoreError.round(2)} ${it[0][1].primaryMetric.scoreUnit}"
+        }
 
 Table.Builder dummyClassTable = new Table.Builder()
         .withAlignments(Table.ALIGN_RIGHT, Table.ALIGN_RIGHT)
         .addRow('JDK name & Version', 'Benchmark results')
-dummyClassResults.forEach { type, results -> dummyClassTable.addRow(type, results) }
+dummyClassResults.sort { a, b -> a.value <=> b.value }.forEach { type, results -> dummyClassTable.addRow(type, results) }
 
 Table.Builder noopTable = new Table.Builder()
         .withAlignments(Table.ALIGN_RIGHT, Table.ALIGN_RIGHT)
         .addRow('JDK name & Version', 'Benchmark results')
-noopResults.forEach { type, results -> noopTable.addRow(type, results) }
+noopResults.sort { a, b -> a.value <=> b.value }.forEach { type, results -> noopTable.addRow(type, results) }
 
 new File('jmh_results.md').text = """
 # `cpw.mods.modlauncher.benchmarks.TransformBenchmark.transformDummyClass` results
