a6317633b289b6272e0f96ce7ebb90efd6a15887
Su5eD <su5ed@outlook.com>
2023-10-01 16:08:49 +0200

    Remove unused additional class/resource locators

--- src/main/java/cpw/mods/modlauncher/TransformationServicesHandler.java
+++ src/main/java/cpw/mods/modlauncher/TransformationServicesHandler.java
@@ -56,7 +56,6 @@ class TransformationServicesHandler {
     }
 
     TransformingClassLoader buildTransformingClassLoader(final LaunchPluginHandler pluginHandler, final TransformingClassLoaderBuilder builder, final Environment environment, final ModuleLayerHandler layerHandler) {
-        final List<Function<String, Optional<URL>>> classLocatorList = serviceLookup.values().stream().map(TransformationServiceDecorator::getClassLoader).filter(Objects::nonNull).collect(Collectors.toList());
         final var layerInfo = layerHandler.buildLayer(IModuleLayerManager.Layer.GAME, (cf, parents)->new TransformingClassLoader(transformStore, pluginHandler, builder, environment, cf, parents));
         layerHandler.updateLayer(IModuleLayerManager.Layer.PLUGIN, li->li.cl().setFallbackClassLoader(layerInfo.cl()));
         return (TransformingClassLoader) layerInfo.cl();
